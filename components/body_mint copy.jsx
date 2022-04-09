193bc9a23aed5446d0098fb5f187d4f659647020
Jaspreet Singh <mail@jas.bio>
2022-04-09 16:06:30 +0400

    update to testnet1

---
+++ components/body_mint copy.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function BodyMint() {
+  return (
+    <>
+      <div>Love is love - NFT Collection</div>
+      <div>10/170</div>
+      <div>1 NFT costs 0.09 ETH</div>
+      <div>Excluding gas fee</div>
+      <div>How many NFTs you like to mint?</div>
+      <div>View on Opensea</div>
+    </>
+  );
+}
+// {/* <ButtonConnect /> */}
